1985fd3477c13f06cfd03aa16c3876d803b66132
asmodeus <tradingasmodeus@gmail.com>
2026-07-10 11:02:23 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/parvathy-web-app/',
+    base: '/Parvathy-Travels-Live-Availability/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
